70c26ba691a299a8c9045df157882065e045e125
robmoore <rob.moore@gmail.com>
2014-07-10 19:08:59 -0500

    Attempt push to github.

---
+++ shared.R
@@ -0,0 +1,27 @@
+maybeDownload.file <- function(dataFileUrl, dataFile) {
+  if (!file.exists(dataFile)) { 
+    download.file(url=dataFileUrl, destfile=dataFile, method="curl") 
+  }
+}
+#Test
+
+# Convert using day/month/year format
+setAs("character","dmyDate", function(from) as.Date(from, format="%d/%m/%Y") )
+
+loadData <- function() {
+  dataFile <- "household_power_consumption.zip"
+  
+  dataFileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  
+  maybeDownload.file(dataFileUrl, dataFile)
+  
+  # Note that in this dataset missing values are coded as ?.
+  read.table(unz(dataFile, "household_power_consumption.txt"), sep = ";", header = TRUE, na.strings = "?", colClasses = c(Date = "dmyDate", "factor", rep("numeric", 7)))
+}
+
+data <- loadData()
+
+# We will only be using data from the dates 2007-02-01 and 2007-02-02. One alternative is to read the data
+# from just those dates rather than reading in the entire dataset and subsetting to those dates.
+data <- data[data$Date %in% as.Date(c('2007-02-01', '2007-02-02')),]
+
